ff8519b3cc6129985ee96a031a3876bbe3b08036
Yogie Putra <yogie.putra8@gmail.com>
2016-05-11 11:26:59 +0700

    add presentation file

--- app_createUser.js
+++ app_createUser.js
@@ -2,15 +2,15 @@ var User = require('./model/user');
 
 
 var newUser = User({
-  name: 'Kevin Faeyza Rasydan',
-  username: 'kevinfay',
-  password: '1234',
-  admin: true
+  name: 'Yogie S P',
+  username: 'yogieputra',
+  password: '12345',
+  admin: false
 });
 
 
 newUser.save(function(err, res, body) {
   if (err) throw err;
 
-  console.log('User created! ' + res.name);
+  console.log('User created! ' + res.username);
 });
